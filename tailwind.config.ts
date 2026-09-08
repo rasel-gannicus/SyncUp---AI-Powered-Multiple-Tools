f4355391f244a760a5b31ca2d40f8e807b68a482
shafik720 <shafikrasel5@gmail.com>
2026-09-08 17:44:13 +0600

    chore: update tailwind content paths to include src, components, and lib directories

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,13 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   darkMode: ["class"],
   content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/Components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/utils/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/lib/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
